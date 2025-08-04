f0579b533e22ef3dfb2357f15b117285ba9c9113
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-08-04 23:57:40 +0600

    home screen navigate

---
+++ app/src/main/java/com/example/ghorongo/ui/screens/homescreen/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.ghorongo.ui.screens.homescreen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Welcome to Home Screen")
+}
+
