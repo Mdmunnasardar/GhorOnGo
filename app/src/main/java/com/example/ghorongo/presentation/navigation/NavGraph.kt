77ef803a36cb03a112ea62feb8b32cc4ac04e0f2
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-08-10 03:05:55 +0600

    bottomnavbar and napGraph initialize

--- app/src/main/java/com/example/ghorongo/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/ghorongo/presentation/navigation/NavGraph.kt
@@ -1,35 +1,28 @@
 package com.example.ghorongo.presentation.navigation
 
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
-import com.example.ghorongo.ui.screens.auth.CheckEmailScreen
-import com.example.ghorongo.ui.screens.auth.ForgotPasswordScreen
-import com.example.ghorongo.ui.screens.auth.LoginScreen
-import com.example.ghorongo.ui.screens.auth.SignUpScreen
+import com.example.ghorongo.ui.screens.booking.SavedScreen
+import com.example.ghorongo.ui.screens.chat.MessageScreen
 import com.example.ghorongo.ui.screens.homescreen.HomeScreen
+import com.example.ghorongo.ui.screens.profile.ProfileScreen
+
+
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "auth") {
-        navigation(startDestination = "login", route = "auth") {
-            composable("login") {
-                LoginScreen(navController = navController)
-            }
-            composable("signup") {
-                SignUpScreen(navController = navController)
-            }
-            composable("forgot_password") {
-                ForgotPasswordScreen(navController = navController)
-            }
-            composable("check_email") {
-                CheckEmailScreen(navController = navController)
-            }
-        }
-        composable("home") {
-            HomeScreen()
-        }
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") { HomeScreen( ) }
+        composable("profile") { ProfileScreen() }
+        composable("saved") { SavedScreen() }
+        composable("message") { MessageScreen() }
     }
 }
+
+
