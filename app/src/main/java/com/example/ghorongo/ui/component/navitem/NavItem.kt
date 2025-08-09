e6af2142073c4f64b6ac0b7e02d67f047ea971b3
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-08-10 03:06:33 +0600

    NavItem.kt created sealed

---
+++ app/src/main/java/com/example/ghorongo/ui/component/navitem/NavItem.kt
@@ -0,0 +1,10 @@
+package com.example.ghorongo.ui.component.navitem
+
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val icon: ImageVector
+)
