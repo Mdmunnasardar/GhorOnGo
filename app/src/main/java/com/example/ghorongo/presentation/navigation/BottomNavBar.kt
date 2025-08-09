e19b40b05d0ab314a625fd54678b43d66b51b670
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-08-10 03:05:15 +0600

    bottom nav bar created

---
+++ app/src/main/java/com/example/ghorongo/presentation/navigation/BottomNavBar.kt
@@ -0,0 +1,29 @@
+package com.example.ghorongo.presentation.navigation
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavBar(navController: NavController, items: List<BottomNavItem>) {
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        items.forEach { item ->
+            NavigationBarItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { Icon(item.icon, contentDescription = item.name) },
+                label = { Text(item.name) }
+            )
+        }
+    }
+}
